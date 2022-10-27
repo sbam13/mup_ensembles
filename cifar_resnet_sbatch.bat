018d60492f579a3352fdcf41f3e30920c1b5d6a0
Sabarish Sainathan <sabarish.sainathan@gmail.com>
2022-10-27 15:58:20 -0400

    lr_exp

--- cifar_resnet_sbatch.bat
+++ cifar_resnet_sbatch.bat
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-rem TODO amend
 #SBATCH --job-name=gpu
 #SBATCH -e slurm-%j.err
 #SBATCH -o slurm-%j.out
@@ -10,21 +9,23 @@ rem TODO amend
 #SBATCH --ntasks=1       
 #SBATCH --gres=gpu:4
 
-rem loads CUDA and conda
+@REM loads CUDA and conda
 module load cuda/11.7.1-fasrc01 cudnn/8.5.0.96_cuda11-fasrc01 Anaconda3/2020.11
 
-rem copies cifar data to scratch for fast access
-CIFAR_FOLDER="/n/holystore01/LABS/pehlevan_lab/Users/sab/cifar-10-batches-py"
-LOCAL_CIFAR_FOLDER = "/tmp/cifar10"
+srun source activate 
+
+@REM rem copies cifar data to scratch for fast access
+@REM CIFAR_FOLDER="/n/holystore01/LABS/pehlevan_lab/Users/sab/cifar-10-batches-py"
+@REM LOCAL_CIFAR_FOLDER = "/tmp/cifar10"
 
 
-mkdir $LOCAL_CIFAR_FOLDER
-cp -R $CIFAR_LOC $LOCAL_CIFAR_FOLDER
+@REM mkdir $LOCAL_CIFAR_FOLDER
+@REM cp -R $CIFAR_LOC $LOCAL_CIFAR_FOLDER
 
-LOCAL_SAVE_FOLDER="/tmp/results"
-mkdir $LOCAL_SAVE_FOLDER
+@REM LOCAL_SAVE_FOLDER="/tmp/results"
+@REM mkdir $LOCAL_SAVE_FOLDER
 
-rem TODO move
-cp -R $LOCAL_SAVE_FOLDER 
+@REM rem TODO move
+@REM cp -R $LOCAL_SAVE_FOLDER 
 
 # Add lines here to run your GPU-based computations.
