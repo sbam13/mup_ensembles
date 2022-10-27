8bad8937110b9c7bb30084d2b01d0c6abb68cb3e
Sabarish Sainathan <sabarish.sainathan@gmail.com>
2022-10-27 16:27:10 -0400

    add conf stuff

---
+++ sbatch_files/cifar_resnet_lr1e-3.bat
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --job-name=lr1e-3
+#SBATCH -e slurm-%j.err
+#SBATCH -o slurm-%j.out
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --mem=64g
+#SBATCH --partition=pehlevan_gpu
+#SBATCH --ntasks=1       
+#SBATCH --gres=gpu:1
+
+@REM loads CUDA and conda
+module load cuda/11.7.1-fasrc01 cudnn/8.5.0.96_cuda11-fasrc01 Anaconda3/2020.11
+
+source activate gs
+
+cd /n/home07/ssainathan/workplace/gpu_scheduler
+
+rm conf/config.yaml
+
+echo "defaults:  - experiment: sweep_dataset_size_16384_lr_0.001" > conf/config.yaml
+
+srun ./main.py
