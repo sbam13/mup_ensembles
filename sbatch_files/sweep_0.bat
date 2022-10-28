6ba4a5310141d94594c5d924c18c962924cf1ecf
Sabarish Sainathan <sabarish.sainathan@gmail.com>
2022-10-28 11:33:48 -0400

    better tmp directory handling

--- sbatch_files/sweep_0.bat
+++ sbatch_files/sweep_0.bat
@@ -14,6 +14,9 @@ module load cuda/11.7.1-fasrc01 cudnn/8.5.0.96_cuda11-fasrc01 Anaconda3/2020.11
 
 source activate gs
 
+rm -rf /tmp/0
+mkdir /tmp/0
+
 mkdir /tmp/0/data-dir
 cp -R "/n/holystore01/LABS/pehlevan_lab/Users/sab/cifar-10-batches-py" /tmp/0/data-dir
 
--- sbatch_files/sweep_5.bat
+++ sbatch_files/sweep_5.bat
@@ -14,6 +14,9 @@ module load cuda/11.7.1-fasrc01 cudnn/8.5.0.96_cuda11-fasrc01 Anaconda3/2020.11
 
 source activate gs
 
+rm -rf /tmp/5
+mkdir /tmp/5
+
 mkdir /tmp/5/data-dir
 cp -R "/n/holystore01/LABS/pehlevan_lab/Users/sab/cifar-10-batches-py" /tmp/5/data-dir
 
