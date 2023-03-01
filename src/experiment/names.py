956e7f45788f4ff1248876d579e1bbf82d01d2f5
Sabarish Sainathan <sabarish.sainathan@gmail.com>
2023-03-01 18:36:50 -0500

    adds imagenet resnet exp

--- src/experiment/names.py
+++ src/experiment/names.py
@@ -1,5 +1,7 @@
 from . import cifar10_resnet
+from . import imagenet_resnet
 
 names = {
-    ('cifar10', 'resnet18'): cifar10_resnet
+    ('cifar10', 'resnet18'): cifar10_resnet,
+    ('imagenet', 'resnet18'): imagenet_resnet
 }
