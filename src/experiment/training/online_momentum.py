d3266b4fbb496ff1199bf041c9433c9f12cd69f9
Sabarish Sainathan <sabarish.sainathan@gmail.com>
2023-03-02 00:44:14 -0500

    16 halved

--- src/experiment/training/online_momentum.py
+++ src/experiment/training/online_momentum.py
@@ -261,7 +261,7 @@ def get_n_ensemble(width):
 
 def get_div_size(N: int, ensemble_size: int):
     div = min(int((2 ** 10) // N), ensemble_size)
-    if N == 128:
+    if N in [16, 128]:
         return div // 2
     else:
         return div
